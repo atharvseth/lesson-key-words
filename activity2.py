9ee6b4bb0f621b47b5db408c25842a8f169e7499
atharvseth <Atharvabhiseth@gmail.com>
2025-11-21 18:55:20 +0530

    key word

---
+++ activity2.py
@@ -0,0 +1,8 @@
+var = 10              # iniotialise
+while var > 0:         # iterate loop
+    var = var - 1
+    if var == 5:      # condition 1 
+        continue      # continue statement
+        # display result
+    print('/current variable value :', var)
+print("\nGOOD NIGHT!!")
